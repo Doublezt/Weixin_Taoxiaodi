f51858b2e09e6de3087e9ef3e8f4e1c2a82d4db3
Doublezt <39691213+Doublezt@users.noreply.github.com>
2018-08-22 22:44:21 +0800

    Taoxiaodi

---
+++ Taoxiaodi/pages/mine/mine.js
@@ -0,0 +1,24 @@
+Page({
+  data: {
+    // text:"这是一个页面"
+    audioPoster: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=1363727097,3810509600&fm=58',
+    audioName: 'Childhood Memory',
+    audioAuthor: '涛',
+    audioSrc: 'http://www.ytmp3.cn/?down/30434.mp3',
+  },
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady: function () {
+    // 页面渲染完成
+  },
+  onShow: function () {
+    // 页面显示
+  },
+  onHide: function () {
+    // 页面隐藏
+  },
+  onUnload: function () {
+    // 页面关闭
+  }
+})
